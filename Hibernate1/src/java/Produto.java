8ba1566a2a366a718925f348bfecd6dad41bf4c4
Rafael Henrique Santos Soares <Rafael.Soares@dinfor.uniceub>
2016-08-02 22:21:13 -0300

    versao com select

---
+++ Hibernate1/src/java/Produto.java
@@ -0,0 +1,49 @@
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rafael.Soares
+ */
+@Entity
+public class Produto {
+    @Id
+    private String nome;
+    private String descricao;
+    private float preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+    
+    
+}
